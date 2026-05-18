7f63ed4a8eb440ffce730399cb34918adfee1ce6
Bingcheng He <32297297+HeBC@users.noreply.github.com>
2026-05-17 22:42:53 -0400

    Remove obsolete constrained Broyden stub

--- MyHF/HartreeFock_Broyden_Constraint.cpp
+++
@@ -1,9 +0,0 @@
-#include "HartreeFock.h"
-
-// The constrained modified-Broyden solver is implemented in
-// HartreeFock_Broyden.cpp through HartreeFock::Solve_broyden_impl(...).
-//
-// This translation unit is intentionally kept as a stub because the makefile
-// still builds HartreeFock_Broyden_Constraint.o.  Keeping an old independent
-// definition of HartreeFock::Solve_broyden_Constraint(...) here causes a linker
-// multiple-definition error.
